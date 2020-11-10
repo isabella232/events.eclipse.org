44eec23b55f9bd0964c76fdbaaa95ad2dac768e1
Eric Poirier <eric.poirier@eclipse-foundation.org>
2020-11-10 11:01:49 -0500

    Add EclipseCon 2008 archived website
    Signed-off-by: Eric Poirier <eric.poirier@eclipse-foundation.org>

---
+++ content/2008/eclipsecon/index.md
@@ -0,0 +1,11 @@
+---
+title: "EclipseCon 2008"
+date: 2008-03-17T08:00:00-04:00
+description: "Conference for the Eclipse Ecosystem"
+page_favicon: "2008/eclipsecon/favicon.ico"
+type: "eclipsecon"
+layout: "section"
+eclipsecon_archive_css: "eclipsecon-2008.css"
+---
+
+{{< eclipsecon/2008 >}}
--- webpack.mix.archives.js
+++ webpack.mix.archives.js
@@ -18,14 +18,10 @@ mix.less('./less/2004/eclipsecon/styles.less', 'static/archives/css/eclipsecon-2
 mix.less('./less/2005/eclipsecon/styles.less', 'static/archives/css/eclipsecon-2005.css');
 mix.less('./less/2006/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2006.css');
 mix.less('./less/2007/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2007.css');
+mix.less('./less/2008/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2008.css');
 mix.less('./less/2015/france/styles.less', 'static/archives/css/eclipsecon-france-2015.css');
 
 /*
-mix.less('./less/2004/styles.less', './content/2004/styles.css');
-mix.less('./less/2005/styles.less', './content/2005/styles.css');
-
-mix.less('./less/2007/styles.less', './content/2007/styles.css');
-mix.less('./less/2008/styles.less', './content/2008/styles.css');
 mix.less('./less/2009/styles.less', './content/2009/styles.css');
 mix.less('./less/2010/styles.less', './content/2010/styles.css');
 mix.less('./less/2011/styles.less', './content/2011/styles.css');
