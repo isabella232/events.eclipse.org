bdcd3a9d59d7b0992a51e75f40684380e2f27190
Yi (Flora) Liu <39588094+flora8984461@users.noreply.github.com>
2020-11-17 17:15:27 -0500

    Add NA 2016 #120 (#144)
    * Add NA 2016 #120
    
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>
    
    * remove unnecessary spacing
    
    Signed-off-by: Yi Liu <yi.liu@eclipse-foundation.org>
    
    Co-authored-by: Christopher Guindon <chris.guindon@eclipse-foundation.org>

---
+++ content/2016/na/index.md
@@ -0,0 +1,12 @@
+---
+title: "EclipseCon North-America 2016"
+date: 2016-03-07T08:00:00-04:00
+description: "Conference for the Eclipse Ecosystem | March 7 - 10, 2016 | Reston, Virginia"
+page_favicon: "2016/na/favicon.png"
+footer_logo: "footer_logo.png"
+type: "eclipsecon"
+layout: "section"
+eclipsecon_archive_css: "na-2016.css"
+---
+
+{{< na/2016 >}}
--- webpack.mix.archives.js
+++ webpack.mix.archives.js
@@ -20,6 +20,7 @@ mix.less('./less/2006/eclipsecon/styles.less',  'static/archives/css/eclipsecon-
 mix.less('./less/2007/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2007.css');
 mix.less('./less/2008/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2008.css');
 mix.less('./less/2009/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2009.css');
+mix.less('./less/2016/na/styles.less',  'static/archives/css/na-2016.css');
 mix.less('./less/2017/europe/styles.less',  'static/archives/css/europe-2017.css');
 mix.less('./less/2013/europe/styles.less',  'static/archives/css/europe-2013.css');
 mix.less('./less/2012/eclipsecon/styles.less',  'static/archives/css/eclipsecon-2012.css');
@@ -52,8 +53,10 @@ mix.less('./less/europe2016/styles.less', './content/europe2016/styles.css');
 mix.less('./less/locationcon2017/styles.less', './content/locationcon2017/styles.css');
 
 mix.less('./less/na2015/styles.less', './content/na2015/styles.css');
+
 mix.less('./less/na2014/styles.less', './content/na2014/styles.css');
 mix.less('./less/na2016/styles.less', './content/na2016/styles.css');
+
 mix.less('./less/na2017/styles.less', './content/na2017/styles.css');
 
 mix.less('./less/summiteurope2007/styles.less', './content/summiteurope2007/styles.css');
